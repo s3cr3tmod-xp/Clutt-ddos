302f7a8b60a8816de1ab896ee269c9d09c9afb37
s3cr3tmod-xp <s3cr3tmod@gmail.com>
2026-04-19 07:26:16 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+    os.system("pip install fake_headers")
+    os.system("pip requests")
+    os.system("pip install click")
+    os.system("pip install colorama")
+elif c == "1":
+    os.system("pip3 install fake_headers")
+    os.system("pip3 install requests")
+    os.system("pip3 install click")
+    os.system("pip3 install colorama")
+print("Done.")
