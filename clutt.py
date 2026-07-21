960a1593164cac46f1ac46be3e3e7a318afef316
s3cr3tmod-xp <s3cr3tmod@gmail.com>
2026-07-21 21:02:50 +0700

    Update clutt.py

--- clutt.py
+++ clutt.py
@@ -86,7 +86,7 @@ def start_ddos(prox, url, headers, proxies, color):
 @click.option('--proxy', '-p', help="File with a proxy")
 @click.option('--url', '-u', help="URL")
 def main(proxy, url):
-	clear()
+	
 	if url == None:
 		url = input("URL: ")
 	if url[:4] != "http":
